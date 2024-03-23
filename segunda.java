c458e5194bb221d4c81cbd6b7f143d317171ef69
JCREYES18 <159776432+JCREYES18@users.noreply.github.com>
2024-03-23 14:02:07 +0100

    Add files via upload

---
+++ segunda.java
@@ -0,0 +1,10 @@
+package trabajoEntornosGitHub;
+
+public class segunda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ tercera.java
@@ -0,0 +1,10 @@
+package trabajoEntornosGitHub;
+
+public class tercera {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
