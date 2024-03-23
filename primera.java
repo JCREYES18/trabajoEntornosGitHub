9e8cd279c39341d8b8f20250ee26768ea1bb9c0b
JCREYES18 <juancarlosreyesgarcia2004@gmail.com>
2024-03-23 14:18:30 +0100

    primer cambio

---
+++ primera.java
@@ -0,0 +1,11 @@
+package trabajoEntornosGitHub;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class primera {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
